eec85e9e6da1170a94d224c4b898aa105b09f1f8
yloeffler <loeffler.yohann@gmail.com>
2017-02-08 15:23:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -32,7 +32,7 @@ if [ "${MYSQL_DB}" != "--all-databases" ]; then
 		BACKUP_CMD="mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} ${EXTRA_OPTS} \${db}"
 
 		echo "=> Backup started: \${BACKUP_NAME}"
-		if \${BACKUP_CMD} > /backup/'\${BACKUP_NAME}' ;then
+		if \${BACKUP_CMD} > /backup/\${BACKUP_NAME} ;then
 		    echo "   Backup succeeded"
 		else
     		echo "   Backup failed"
@@ -45,7 +45,7 @@ else
 	BACKUP_CMD="mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} ${EXTRA_OPTS} \${MYSQL_DB}"
 
 	echo "=> Backup started: \${BACKUP_NAME}"
-	if \${BACKUP_CMD} > /backup/'\${BACKUP_NAME}' ;then
+	if \${BACKUP_CMD} > /backup/\${BACKUP_NAME} ;then
 	    echo "   Backup succeeded"
 	else
     	echo "   Backup failed"
