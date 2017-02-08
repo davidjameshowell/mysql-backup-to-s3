6281d9acd608867b866f0d7da1914f4da5ff7b2d
yloeffler <loeffler.yohann@gmail.com>
2017-02-08 14:20:14 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ if [ "${MYSQL_DB}" != "--all-databases" ]; then
 	for db in ${MYSQL_DB}
 	do
 		BACKUP_NAME=\${db}-\$(date +\%Y\%m\%d-\%H\%M\%S).sql
-		BACKUP_CMD="mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} ${EXTRA_OPTS} -B \${db} > /backup/"\${BACKUP_NAME}
+		BACKUP_CMD="mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} ${EXTRA_OPTS} \${db} > /backup/"\${BACKUP_NAME}
 
 		echo "=> Backup started: \${BACKUP_NAME}"
 		if \${BACKUP_CMD} ;then
